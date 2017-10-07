27edff4c8110539c3ed54422d2a876853f1296bd
dmnbc <dmnbc@detlef-kremer.com>
2017-10-07 16:03:07 +0200

    erstes ablauffähiges C# Programm

--- HelloWorld_ConsoleApplication/HelloWorld_ConsoleApplication/Program.cs
+++ HelloWorld_ConsoleApplication/HelloWorld_ConsoleApplication/Program.cs
@@ -1,10 +1,10 @@
-﻿using System;
+﻿using System;  // notwendige Bibliothek für Consolenbefehle, z.B. WrieLine
 
 namespace HelloWorld_ConsoleApplication
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()   // Startpunkt der .exe Datei
         {
             Console.WriteLine("HellWorld");
             Console.ReadLine();
