e5fc50aab094d39611fd96223747958a3d99d5d7
dmnbc <dmnbc@detlef-kremer.com>
2017-10-07 17:25:22 +0200

    Console Class Beispiele

---
+++ classConsole_ConsoleApplication/classConsole_ConsoleApplication/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace classConsole_ConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.WriteLine("The current window width is {0} Chars",Console.WindowWidth);
+            Console.ReadLine();
+            Console.SetWindowSize(80, 40);
+            Console.BackgroundColor = ConsoleColor.DarkGreen;
+            Console.WriteLine("The current window width is {0} Chars", Console.WindowWidth);
+            Console.ReadLine();
+           
+        }
+    }
+}
---
+++ classConsole_ConsoleApplication/classConsole_ConsoleApplication/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Allgemeine Informationen über eine Assembly werden über die folgenden 
+// Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
+// die einer Assembly zugeordnet sind.
+[assembly: AssemblyTitle("classConsole_ConsoleApplication")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("classConsole_ConsoleApplication")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Durch Festlegen von ComVisible auf "false" werden die Typen in dieser Assembly unsichtbar 
+// für COM-Komponenten.  Wenn Sie auf einen Typ in dieser Assembly von 
+// COM aus zugreifen müssen, sollten Sie das ComVisible-Attribut für diesen Typ auf "True" festlegen.
+[assembly: ComVisible(false)]
+
+// Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
+[assembly: Guid("f454ac04-2cbe-45ce-aba8-42d6c596f4bd")]
+
+// Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
+//
+//      Hauptversion
+//      Nebenversion 
+//      Buildnummer
+//      Revision
+//
+// Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
+// übernehmen, indem Sie "*" eingeben:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
