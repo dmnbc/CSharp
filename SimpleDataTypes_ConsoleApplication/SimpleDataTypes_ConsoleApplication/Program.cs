2587530ed3b5af268ca6cd36269df18cef13cacf
dmnbc <dmnbc@detlef-kremer.com>
2017-10-07 16:16:22 +0200

    programmcide mit einer Variablen

---
+++ SimpleDataTypes_ConsoleApplication/SimpleDataTypes_ConsoleApplication/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleDataTypes_ConsoleApplication
+{
+    class Program
+    {
+        static string s = "Hello";
+        static void Main(string[] args)
+        {
+            Console.WriteLine(s);
+        }
+    }
+}
