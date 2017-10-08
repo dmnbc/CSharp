e01b73e460433c01619ec9071500101e4697d1be
dmnbc <dmnbc@detlef-kremer.com>
2017-10-08 14:53:04 +0200

    Versetzen der Schreibmarke nach Programmstart

--- CurosrPositioning_ConsoleApplication/CurosrPositioning_ConsoleApplication/Program.cs
+++ CurosrPositioning_ConsoleApplication/CurosrPositioning_ConsoleApplication/Program.cs
@@ -1,15 +1,20 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using static System.Console;
 
 namespace CurosrPositioning_ConsoleApplication
 {
     class Program
     {
+        static int origRow, origCol;
         static void Main(string[] args)
         {
+            Clear();
+            origRow = CursorTop;
+            origCol = CursorLeft;
+            WriteLine(" Console.CursorLeft und Console.CursorTop befinden sich nach einem 'Clear' bei {0},{1}", origRow, origCol);
+            Console.SetCursorPosition(10,10);
+            WriteLine("Mit dem 'SetCursorPosition(left,top)' Befehl kann frei entschieden werden, ab wo die nächste Ausgabe erfolgt");
+            ReadKey();
         }
     }
 }
