31e03b4876abccfd4b4223f1a25aa81eeb8985ee
dmnbc <dmnbc@detlef-kremer.com>
2017-10-08 17:09:01 +0200

    EinAusgabe mit Read, Readline und Interpolation

---
+++ EinAusgabe_ConsoleApplication/EinAusgabe_ConsoleApplication/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using static System.String;
+using static System.Console;
+
+namespace EinAusgabe_ConsoleApplication
+{
+    class Program
+    {
+        static void Main()
+        {
+            int eingelesen = Read();
+            WriteLine($"von der Tastatur wurde durch ReadKey {eingelesen} eingelesen.");
+            ReadLine();
+            WriteLine(new string('-', 20));
+            string aucheingelesen = ReadLine();
+            WriteLine($"von der Tastatur wurde durch ReadLine {aucheingelesen} eingelesen.");
+            ReadLine();
+        }
+    }
+}
