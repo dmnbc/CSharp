e7947025a10e0a5452c15738678bbc8ed4daba6b
dmnbc <dmnbc@detlef-kremer.com>
2017-10-22 10:39:00 +0200

    Basisbeziehungen der Klassen

---
+++ DiscounterActor_ConsoleApplication/DiscounterActor_ConsoleApplication/Personal.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DiscounterActor_ConsoleApplication
+{
+     class Personal : Actor
+    {
+    }
+}
