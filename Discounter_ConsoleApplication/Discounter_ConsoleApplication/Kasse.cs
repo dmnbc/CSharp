5e3f15fd9b1bcebedfbeb1f0e57fbe8ba5b9a0b5
dmnbc <dmnbc@detlef-kremer.com>
2017-10-25 14:54:26 +0200

    Kasse Rohentwurf

---
+++ Discounter_ConsoleApplication/Discounter_ConsoleApplication/Kasse.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscounterActor_ConsoleApplication
+{
+    class Kasse  // soll den Umsatz berechnen für jeden Kunden
+                 // für den ganzen Tag 
+                 // Bestandsabfragen ( pro Regal) ermöglichen
+                 // Schwund ermitteln 
+    {
+        // Attribute
+        private double kunde_umsatz;
+        private double tages_umsatz;
+
+        // Methoden
+
+        public Kasse()
+        {
+
+        }
+
+        public double kunde_abrechnen(/* einkaufswagen und warenkatalog */)
+        {
+            tages_umsatz += 11.22;  /* summe der artikelmenge * preis */;
+            return 123.44; /* summe der artikelmenge * preis */;
+        }
+
+        public Einkaufszettel nachfuellen_anfordern()
+        {
+            return  ; 
+        }
+
+        public Einkaufszettel  fehlbestand_anzeigen()
+        {
+            return;
+        }
+    }
+}
