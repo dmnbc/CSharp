64bf086108c134af666f8118d6dd46c19e2d7d24
dmnbc <dmnbc@detlef-kremer.com>
2017-11-03 09:31:42 +0100

    Rohmaterial der Ortsangaben

---
+++ csvVerarbeitung_ConsoleApplication/csvVerarbeitung_ConsoleApplication/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csvVerarbeitung_ConsoleApplication
+{
+    class Program
+    {
+   
+
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
