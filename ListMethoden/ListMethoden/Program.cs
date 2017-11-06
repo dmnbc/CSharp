3e22e24229dd6bf48a1f72417e3c54b27522db83
dmnbc <dmnbc@detlef-kremer.com>
2017-11-06 10:17:45 +0100

    fertige Lösung mit List Add und RemoveAt

--- ListMethoden/ListMethoden/Program.cs
+++ ListMethoden/ListMethoden/Program.cs
@@ -18,31 +18,45 @@ namespace ListMethoden
 
         public void entladen() // letztes Teil wieder abladen
             {
-                Console.WriteLine(" Momentangewicht {0} ", Momentangewicht());
+                
                 frachtliste.RemoveAt(frachtliste.Count() - 1);
-                Console.WriteLine(" Momentangewicht nach dem Abladen {0} ", Momentangewicht());
+                Ladeliste();
+               
             }
 
         public void beladen(double einzelStueckGewicht)
             {
-                frachtliste.Add(einzelStueckGewicht);
-                Console.WriteLine("Es wird ein weiteres Teil mit {0} Tonnen aufgeladen",einzelStueckGewicht);
-                if(Momentangewicht() >= maxGewicht)
+                
+                Ladeliste();
+                if(Momentangewicht()+einzelStueckGewicht <= maxGewicht)
                 {
-                    Console.WriteLine("Das Fahrzeug wäre mit {0}  überladen!",Momentangewicht());
-                    entladen();
+                    frachtliste.Add(einzelStueckGewicht);
+
                 }
                 else
                 {
-                    
+                    Console.WriteLine("Das letzte Frachtstück konnte wegen Überschreitung des Gesamtgewichts nicht geladen werden ");
                 }
+
+
             }
 
             public double Momentangewicht()
             {
-                foreach(double einzel in frachtliste)
+                momentanGewicht = 0;
+                foreach (double einzel in frachtliste)
                 { momentanGewicht += einzel; }
                 return momentanGewicht;
+            }
+
+            public void Ladeliste()
+            {
+                int i = 0;
+                Console.WriteLine("Aktuelle Ladeliste : ");
+                foreach (double einzel in frachtliste)
+                { Console.WriteLine("{0}\t{1}", i++, einzel); }
+                Console.WriteLine("Gesamtgewicht dieser Frachtliste {0}", Momentangewicht());
+
             }
 
 
@@ -51,11 +65,24 @@ namespace ListMethoden
         {
 
             LKW_Ladung HH_M = new LKW_Ladung();
-            HH_M.beladen(1.0);
-            HH_M.beladen(2.0);
-            HH_M.beladen(3.0);
-            HH_M.beladen(2.0);
-            HH_M.beladen(1.0);
+            HH_M.beladen(1.40);
+
+            HH_M.beladen(2.60);
+            HH_M.beladen(2.60);
+            HH_M.beladen(.75);
+            HH_M.beladen(.4);  // geht nicht mehr
+            HH_M.entladen();   // dann ist der.75 runter
+            HH_M.beladen(.4);
+            HH_M.Ladeliste();
+
+
+
+
+
+
+            /*   HH_M.beladen(3.0);
+               HH_M.beladen(2.0);
+               HH_M.beladen(1.0); */
 
 
             Console.ReadLine();
