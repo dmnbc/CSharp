a098cc261871b0a6fbb932c31667b190e066aef2
dmnbc <dmnbc@detlef-kremer.com>
2017-11-06 13:23:17 +0100

    Mit Abladen passender Frachtstücke

--- ListMethoden/ListMethoden/Program.cs
+++ ListMethoden/ListMethoden/Program.cs
@@ -24,7 +24,15 @@ namespace ListMethoden
                
             }
 
-        public void beladen(double einzelStueckGewicht)
+            public void entladen(int i) // bestimmtes Teil wieder abladen
+            {
+
+                frachtliste.RemoveAt(i);
+                Ladeliste();
+
+            }
+
+            public void beladen(double einzelStueckGewicht)
             {
                 
                 Ladeliste();
@@ -35,10 +43,35 @@ namespace ListMethoden
                 }
                 else
                 {
-                    Console.WriteLine("Das letzte Frachtstück konnte wegen Überschreitung des Gesamtgewichts nicht geladen werden ");
+                    Console.WriteLine("Das letzte Frachtstück kann wegen Überschreitung des Gesamtgeichts nicht aufgeladen werden.");
+                    Console.WriteLine("Wollen Sie dafür ein anderes Teil abladen lassen?");
+                    Console.WriteLine("Sie können den Index des Frachtstückes als Ganzzahl angeben ");
+                    Console.WriteLine("Oder eine Zahl mit Komma, die das zu entfernende Gewicht angibt");
+
+                    int eingabeIndex;
+                    string input;
+                    input = Console.ReadLine();
+                    if (int.TryParse(input, out eingabeIndex))
+                        {
+                        Console.WriteLine("Das Frachstück index {0} wird abgeladen", eingabeIndex);
+                        frachtliste.RemoveAt(eingabeIndex);
+                    }
+                    else
+                    {
+                        double eingabeGewicht;
+                        double.TryParse(input, out eingabeGewicht);
+                        Console.WriteLine("Es wird ein Frachstück mit mehr als {0} Tonnen abgeladen", eingabeGewicht);
+                        int foundAT = -1;
+                        int index = 0;
+                        foreach (double gewicht in frachtliste)
+                        { if (gewicht >= eingabeGewicht)
+                                { foundAT = index; }
+                            index++;
+                        }
+                        frachtliste.RemoveAt(foundAT);
+                    }
+                    frachtliste.Add(einzelStueckGewicht);
                 }
-
-
             }
 
             public double Momentangewicht()
@@ -65,18 +98,31 @@ namespace ListMethoden
         {
 
             LKW_Ladung HH_M = new LKW_Ladung();
-           
+            double eingabe;
 
+            Console.WriteLine("Bitte geben Sie die Gewichte Ihrer Frachtstücke an");
+            Console.WriteLine("Um die Eingabe zu beenden, geben Sie 'Ende' ein");
+
+
+            while ( double.TryParse(Console.ReadLine(),out eingabe))
+            {
+                HH_M.beladen(eingabe);
+                HH_M.Ladeliste();
+            }
 
             //   Aufgabe 
             // eingaben von Frachtstücken durch den Anwender
 
             // wenn Gesamtgewicht überschritten, soll er eine Angabe machen können,
 
-            // entweder, welches Stück wieder abgelden wird.
+            // entweder, welches Stück wieder abgeladen wird.
+
+               // RemoveAt()    
 
             // oder  er nennt ein Gewicht, dass abgeladen werden soll.
             // entfernt werden soll dann ein Frachtstücke, dass mindestens x wiegt.
+
+              // Remove( gewicht )  
 
 
 
