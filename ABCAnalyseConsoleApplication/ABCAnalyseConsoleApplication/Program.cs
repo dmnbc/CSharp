b9501f82a350f968c10bc1794041e79b35a4d53b
dmnbc <dmnbc@detlef-kremer.com>
2017-11-06 15:23:38 +0100

    analyse mit meist genannten

---
+++ ABCAnalyseConsoleApplication/ABCAnalyseConsoleApplication/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ABCAnalyseConsoleApplication
+{
+    class Program
+    {
+        static public List<char> nennungen = new List<char>(){ 'a', 'b', 'C', 'Z', 'j', 'b', 'C', 'Z',
+       'a', 'b', 'C', 'Z', 'b', 'C', 'Z', 'b', 'C', 'Z', 'b', 'C', 'Z', 'b', 'C', 'Z', 'b', 'C', 'Z','b' };
+
+
+
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("Anzahl der Nennungen: {0} ", nennungen.Count);
+
+            var query = (from anzahl in nennungen
+                         group anzahl by anzahl into anzahl1
+
+                         orderby (anzahl1.Count()) descending
+
+                         select new { treffer = anzahl1.Count(), wer = anzahl1.First() }).Take(1)
+                         ;
+
+
+            foreach( var item in query )
+            { Console.WriteLine("{0}: {1}",item.treffer,item.wer); }
+
+            Console.ReadLine();
+
+        }
+    }
+}
