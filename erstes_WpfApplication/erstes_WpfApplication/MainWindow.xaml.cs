90a80cf8347ed78975eaec1461c282f04e215537
Detlef Kremer <github@fachausbilder.net>
2017-11-10 07:53:41 +0100

    Add files via upload

---
+++ erstes_WpfApplication/erstes_WpfApplication/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace erstes_WpfApplication
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public static bool button_switch;
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            
+            Button clickedButton = (Button)sender;
+            if (button_switch)
+            {
+                clickedButton.Content = "auf Verkaufsraum umschalten";
+                textBlock.Text = "Das Lager";
+                button_switch = false;
+                Background = new SolidColorBrush(Colors.Red);
+
+            }
+            else
+            {
+                clickedButton.Content = "auf Lageransicht umschalten";
+                textBlock.Text = "Der Verkaufsraum";
+                button_switch = true;
+                Background = new SolidColorBrush(Colors.Aqua);
+            }
+
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            button_switch = true;
+            button.Click += Button_Click;
+       
+
+        }
+
+       
+
+       
+
+    }
+}
