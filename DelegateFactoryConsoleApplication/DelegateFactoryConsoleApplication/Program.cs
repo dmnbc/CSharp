045ac5b0bf75f8ee6b082e39fa022d0437e4d284
dmnbc <dmnbc@detlef-kremer.com>
2017-11-13 14:49:54 +0100

    lösung mit lambda

--- DelegateFactoryConsoleApplication/DelegateFactoryConsoleApplication/Program.cs
+++ DelegateFactoryConsoleApplication/DelegateFactoryConsoleApplication/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,8 +9,27 @@ namespace DelegateFactoryConsoleApplication
 {
     class Program
     {
+        public delegate void DelArbeitsschritte(string auftrag);
         static void Main(string[] args)
         {
+            DelArbeitsschritte Auto_bauen = new DelArbeitsschritte( (string a) => { });
+
+            List<DelArbeitsschritte> einzelJobs = new List<DelArbeitsschritte>()
+                {StepsToBuild.rohKarrosse_bereitstellen,
+                 StepsToBuild.motor_bereitstellen,
+                 StepsToBuild.achsen_montieren,
+                 StepsToBuild.motor_einsetzen,
+                 StepsToBuild.Achsen_Getriebe_verbinden};      
+           
+            foreach(var step in einzelJobs)
+            {
+                Auto_bauen += step;
+            }
+            Auto_bauen("VW Golf ");
+
+
+
+            Console.ReadLine();
         }
     }
 }
