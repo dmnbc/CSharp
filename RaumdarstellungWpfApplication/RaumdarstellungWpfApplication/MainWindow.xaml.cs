2a5c63b88eca14b49f1eb572810e2856bcd1ef16
dmnbc <dmnbc@detlef-kremer.com>
2017-11-29 11:54:44 +0100

    jetzt mit Punkten der Gangeingänge

--- RaumdarstellungWpfApplication/RaumdarstellungWpfApplication/MainWindow.xaml.cs
+++ RaumdarstellungWpfApplication/RaumdarstellungWpfApplication/MainWindow.xaml.cs
@@ -7,6 +7,7 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
+using System.Windows.Ink;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
@@ -20,6 +21,7 @@ namespace RaumdarstellungWpfApplication
     /// </summary>
     public partial class MainWindow : Window
     {
+        public static List<Label> gangbei = new List<Label>(); // soll das Abbiegen in die Gänge steuern
         public void mover(object sender, RoutedEventArgs e)
         {
             LeftDock.Width = 300;
@@ -49,7 +51,8 @@ namespace RaumdarstellungWpfApplication
                 StackPanel gangVor = new StackPanel();
                 gangVor.Orientation = System.Windows.Controls.Orientation.Vertical;
                 Label leerVorO = new Label { Content = " " }; leerVorO.Background = Brushes.Red;
-                Label leerVorM = new Label { Content = " " }; leerVorM.Background = Brushes.Red;
+                Label leerVorM = new Label { Content = " ",Name="GA"+i }; leerVorM.Background = Brushes.Red;
+                gangbei.Add(leerVorM);
                 Label leerVorU = new Label { Content = " " }; leerVorU.Background = Brushes.Red;
                 gangVor.Children.Add(leerVorO);
                 gangVor.Children.Add(leerVorM);
@@ -61,6 +64,7 @@ namespace RaumdarstellungWpfApplication
                     rgr.Orientation = System.Windows.Controls.Orientation.Vertical;
                     Label regal_l = new Label { Content = "R"+ ((i*40+j)*2).ToString("D3") };
                     Label leer    = new Label { Content = " " }; leer.Background = Brushes.Red;
+                       
                     Label regal_r = new Label { Content = "R"+(((i * 40 + j) * 2)+1).ToString("D3") };
                     rgr.Children.Add(regal_l);
                     rgr.Children.Add(leer);
@@ -84,7 +88,23 @@ namespace RaumdarstellungWpfApplication
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
+            //  double pos;
 
+            foreach (Label g in gangbei)
+            {
+                Point relativePoint = g.TransformToAncestor(Application.Current.MainWindow).Transform(new Point(0, 0));
+                MessageBox.Show(relativePoint.ToString());
+                Ellipse kundenAlsKreis = new Ellipse
+                {
+                    Width = 10,
+                    Height = 10,
+                    Fill = Brushes.Black  
+                                                     
+                };
+                canvas.Children.Add(kundenAlsKreis);
+                Canvas.SetTop(kundenAlsKreis, relativePoint.Y);
+                Canvas.SetLeft(kundenAlsKreis, relativePoint.X);
+            }
         }
     }
 }
