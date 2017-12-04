9cfee3a12edf0ae0dfe1ae907dbbfb27499d7769
dmnbc <dmnbc@detlef-kremer.com>
2017-12-04 14:47:08 +0100

    mit Objekt am Cursor

--- WunschListeWpfApplication/WunschListeWpfApplication/MainWindow.xaml.cs
+++ WunschListeWpfApplication/WunschListeWpfApplication/MainWindow.xaml.cs
@@ -20,6 +20,8 @@ namespace WunschListeWpfApplication
     /// </summary>
     public partial class MainWindow : Window
     {
+        public static double summe = 0;
+        
         public MainWindow()
         {
             InitializeComponent();
@@ -32,11 +34,13 @@ namespace WunschListeWpfApplication
             Things wunsch = listbox.SelectedItem as Things;
             DataObject data = new DataObject(wunsch);
             DragDrop.DoDragDrop(listbox, data, DragDropEffects.Copy);
+                
         }
 
         private void Canvas_DragEnter(object sender, DragEventArgs e)
         {
             wunschzettel.Background = Brushes.DarkGoldenrod;
+           ;
         }
         private void Canvas_DragLeave(object sender, DragEventArgs e)
         {
@@ -45,15 +49,28 @@ namespace WunschListeWpfApplication
 
         private void wunschzettel_DragOver(object sender, DragEventArgs e)
         {
-            ;
+             int ele = wunschzettel.Children.Count;
+        Things kopie = (Things)e.Data.GetData(typeof(Things));
+          
+            Point p = e.GetPosition(wunschzettel);
+            label.Content = p.X.ToString() +" : "+ p.Y.ToString();
+      
+            ContentControl cc = new ContentControl();
+            cc.Content = kopie;
+            Canvas.SetLeft(cc, p.X);
+            Canvas.SetTop(cc, p.Y);
+            wunschzettel.Children.Add(cc);
+            if(wunschzettel.Children.Count > 1)
+            wunschzettel.Children.RemoveAt(ele-1);
+
         }
 
         private void wunschzettel_Drop(object sender, DragEventArgs e)
         {
          
             Things kopie = (Things)e.Data.GetData(typeof(Things));
-            Canvas canvas = e.Source as Canvas;
-            Point p = e.GetPosition(canvas);     // wo ist die Maus beim Drop
+         //   Canvas canvas = e.Source as Canvas;
+            Point p = e.GetPosition(wunschzettel);     // wo ist die Maus beim Drop
 
             ContentControl cc = new ContentControl();
             cc.Content = kopie;
@@ -61,10 +78,11 @@ namespace WunschListeWpfApplication
             Canvas.SetLeft(cc, p.X);
             Canvas.SetTop(cc,  p.Y);
             wunschzettel.Children.Add(cc);
+            summe += kopie.preis;
+            label.Content = summe;
 
 
 
-
-        }
+        } 
     }
 }
