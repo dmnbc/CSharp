b0721a36d8bbbdfd7ce13701e1f94868f8983854
dmnbc <dmnbc@detlef-kremer.com>
2017-12-06 13:52:15 +0100

    entfernung

--- MapWpfApplication/MapWpfApplication/MainWindow.xaml.cs
+++ MapWpfApplication/MapWpfApplication/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using static System.Math;
 
 namespace MapWpfApplication
 {
@@ -20,9 +21,25 @@ namespace MapWpfApplication
     /// </summary>
     public partial class MainWindow : Window
     {
+        public static List<Point> koord = new List<Point>();
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Image_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            // MessageBox.Show
+
+            koord.Add(e.GetPosition(Application.Current.MainWindow));
+            if(koord.Count == 2)
+            {
+                double entfernung;
+                entfernung = Math.Sqrt(Math.Pow(Math.Abs(koord[0].X - koord[1].X), 2)
+                                     + Math.Pow(Math.Abs(koord[0].Y - koord[1].Y), 2));
+                MessageBox.Show(entfernung.ToString());
+            }
+
         }
     }
 }
